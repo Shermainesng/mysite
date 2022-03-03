f04d39920163d0c18c7c80b5d593fbf4c2d96a8a
Shermainesng <shersmallsng@gmail.com>
2022-03-03 23:07:29 +0800

    new links

--- app/javascript/myreact/components/Contact.js
+++ app/javascript/myreact/components/Contact.js
@@ -5,33 +5,36 @@ import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
-    <div id="contact">
-      <div className="container-fluid container-contact">
-        <div className="row justify-content-between align-items-center row-custom-contact">
-          <div className="social col-xs-12 col-lg-4">
-            <ul className="d-flex flex-row m-0 justify-content-evenly p-0">
-              <li>
-                <a target="_blank" href="https://linkedin.com/in/shermainesng005">
-                  <span className="media-link">LinkedIn</span>
-                </a>
-              </li>
-              <li>
-                <a target="_blank" href="https://github.com/Shermainesng">
-                  <span className="media-link">Github</span>
-                </a>
-              </li>
-              <li>
-                <a target="_blank" href="https://medium.com/@shermainesng">
-                  <span className="media-link">Medium</span>
-                </a>
-              </li>
-              <li>
-                <a target="_blank" href="mailto:sngshermaine98@gmail.com?subject=Let's Talk">
-                  <FontAwesomeIcon className="envelope" icon={faEnvelopeOpen} />
-                </a>
-              </li>
-            </ul>
-          </div>
+    <div className="container-fluid">
+      <div className="row social">
+        <div className="col-sm-10 col-lg-6">
+          <ul className="nav row-custom-contact justify-content-start align-items-center ps-3">
+            <li className="nav-item pe-2">
+              <a target="_blank" href="https://linkedin.com/in/shermainesng005">
+                <span className="media-link">LinkedIn</span>
+              </a>
+            </li>
+            <li className="nav-item pe-2">
+              <a target="_blank" href="https://github.com/Shermainesng">
+                <span className="media-link">Github</span>
+              </a>
+            </li>
+            <li className="nav-item pe-2">
+              <a target="_blank" href="https://www.frontendmentor.io/profile/Shermainesng/solutions">
+                <span className="media-link">Frontend Designs</span>
+              </a>
+            </li>
+            <li className="nav-item pe-2">
+              <a target="_blank" href="https://medium.com/@shermainesng">
+                <span className="media-link">Medium</span>
+              </a>
+            </li>
+            <li className="nav-item pe-2">
+              <a target="_blank" href="mailto:sngshermaine98@gmail.com?subject=Let's Talk">
+                <FontAwesomeIcon className="envelope" icon={faEnvelopeOpen} />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -39,3 +42,41 @@ function Contact() {
 }
 
 export default Contact;
+
+{
+  /* // <div id="contact">
+//   <div className="container-fluid container-contact">
+//     <div className="row justify-content-between align-items-center row-custom-contact">
+//       <div className="social col-xs-12 col-lg-4">
+//         <ul className="d-flex flex-row m-0 justify-content-evenly p-0">
+//           <li>
+//             <a target="_blank" href="https://linkedin.com/in/shermainesng005">
+//               <span className="media-link">LinkedIn</span>
+//             </a>
+//           </li>
+//           <li>
+//             <a target="_blank" href="https://github.com/Shermainesng">
+//               <span className="media-link">Github</span>
+//             </a>
+//           </li>
+//           <li>
+//             <a target="_blank" href="https://medium.com/@shermainesng">
+//               <span className="media-link">Medium</span>
+//             </a>
+//           </li>
+//           <li>
+//             <a target="_blank" href="https://www.frontendmentor.io/profile/Shermainesng/solutions">
+//               <span className="media-link">Frontend Designs</span>
+//             </a>
+//           </li>
+//           <li>
+//             <a target="_blank" href="mailto:sngshermaine98@gmail.com?subject=Let's Talk">
+//               <FontAwesomeIcon className="envelope" icon={faEnvelopeOpen} />
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   </div>
+// </div> */
+}
